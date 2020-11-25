67276905c99d2557d711890b03e6770ed7a267a8
Benjamin Gambill <bgambill@uncc.edu>
2020-11-25 15:03:11 -0500

    Made slight changes to bubblechart

--- Visualization Lab/bubblechart.py
+++ Visualization Lab/bubblechart.py
@@ -13,7 +13,7 @@ go.Scatter(x=df['average_min_temp'],
 y=df['average_max_temp'],
 text=df['month'],
 mode='markers',
-marker=dict(size=df['actual_mean_temp'] / 5,color=df['average_max_temp'], showscale=True))
+marker=dict(size=df['actual_mean_temp'] / 5,color=df['actual_mean_temp'], showscale=True))
 ]
 
 # Preparing layout
