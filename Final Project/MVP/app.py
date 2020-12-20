4d08bd92d3bb5396115fa848371fe82d340a9b39
Scott Girard <sgirard@uncc.edu>
2020-12-20 16:11:40 -0500

    add viewport meta

--- Final Project/MVP/app.py
+++ Final Project/MVP/app.py
@@ -1,4 +1,6 @@
 import dash
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, serve_locally=True)
+app = dash.Dash(__name__, suppress_callback_exceptions=True, serve_locally=True, meta_tags=[
+    {'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}
+    ])
 server = app.server
